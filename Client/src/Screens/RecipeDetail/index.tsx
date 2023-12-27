5299b35182ab3f7e8cb7ec9e78ad3411881d3415
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-12-27 23:06:30 +0700

    reset

--- Client/src/Screens/RecipeDetail/index.tsx
+++ Client/src/Screens/RecipeDetail/index.tsx
@@ -7,7 +7,7 @@ export const RecipeDetail = () => {
 
     const route = useRoute()
 
-    const {name, owner, ownerAvatar, like, heart, clap, imgUrl, ingredient} = route.params.recipe
+    const {name, owner, ownerAvatar, like, heart, clap, imgUrl, ingredient} = route?.params?.recipe
     return (
         <Text>
             {name && name}
